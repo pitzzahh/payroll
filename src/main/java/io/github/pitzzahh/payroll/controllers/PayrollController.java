09c8b0fd94b43ea2bea3898b0d7b6fd23f288081
pitzzahh <araopj2191@gmail.com>
2022-11-08 09:32:19 +0800

    fixed some bug

--- src/main/java/io/github/pitzzahh/payroll/controllers/PayrollController.java
+++ src/main/java/io/github/pitzzahh/payroll/controllers/PayrollController.java
@@ -11,6 +11,15 @@ public class PayrollController {
 
     @FXML
     public void onMouseEnteredHoursWorkedInput(MouseEvent mouseEvent) {
+        logic(mouseEvent, false);
+    }
+
+    /**
+     * Contains the logic.
+     * @param mouseEvent the mouse event.
+     * @param isAbsences if choice box is from absences.
+     */
+    private static void logic(MouseEvent mouseEvent, boolean isAbsences) {
         Object selectedItem = getSelectionModel(mouseEvent, false)
                 .getSelectedItem();
         TextField textField = getTextField(mouseEvent, false);
@@ -21,7 +30,8 @@ public class PayrollController {
                         mouseEvent,
                         false
                 ).getSelectedItem(),
-                textField
+                textField,
+                isAbsences
         );
     }
 
@@ -34,8 +44,25 @@ public class PayrollController {
         Object selectedItem = getSelectionModel(mouseEvent, true).getSelectedItem();
         TextField textField = getTextField(mouseEvent, true);
         String hoursWorked = textField.getText().trim();
-        addHoursWorked(selectedItem, hoursWorked);
+        addHours(selectedItem, hoursWorked, false);
         System.out.println(getHoursWorkedPerMonth());
+        textField.setText("");
+        getSelectionModel(mouseEvent, true)
+                .select(getSelectionModel(mouseEvent, true).getSelectedIndex() + 1);
+    }
+
+    @FXML
+    public void onMouseEnteredAbsencesPerMonth(MouseEvent mouseEvent) {
+        logic(mouseEvent, true);
+    }
+
+    @FXML
+    public void onSaveAbsences(MouseEvent mouseEvent) {
+        Object selectedItem = getSelectionModel(mouseEvent, true).getSelectedItem();
+        TextField textField = getTextField(mouseEvent, true);
+        String absentHours = textField.getText().trim();
+        addHours(selectedItem, absentHours, true);
+        System.out.println(getHoursAbsencesPerMonth());
         textField.setText("");
         getSelectionModel(mouseEvent, true)
                 .select(getSelectionModel(mouseEvent, true).getSelectedIndex() + 1);
